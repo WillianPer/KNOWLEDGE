9816ee645e9a9d5e4575ac6acf40632d845824f4
WilianPer <wsresilienttasteshopw@gmail.com>
2022-11-19 16:43:50 -0400

    API de Categoria #2!!

--- backend/api/category.js
+++ backend/api/category.js
@@ -2,16 +2,21 @@ module.exports = app => {
     const { existsOrError, notExistsOrError } = app.api.validation
 
     const save = (req, res) => {
-        const category = { ...req.body }
+        const category = {
+            id: req.body.id,
+            name: req.body.name,
+            parentId: req.body.parentId
+        }
+        
         if(req.params.id) category.id = req.params.id
 
         try {
-            existsOrError(category.name, 'Nome não informado!!')
-        } catch (msg) {
+            existsOrError(category.name, 'Nome não informado')
+        } catch(msg) {
             return res.status(400).send(msg)
         }
 
-        if (category.id) {
+        if(category.id) {
             app.db('categories')
                 .update(category)
                 .where({ id: category.id })
@@ -23,15 +28,15 @@ module.exports = app => {
                 .then(_ => res.status(204).send())
                 .catch(err => res.status(500).send(err))
         }
-    } 
+    }
 
     const remove = async (req, res) => {
         try {
-            existsOrError(req.params.id, 'Código da categoria não informado.')
+            existsOrError(req.params.id, 'Código da Categoria não informado.')
 
             const subcategory = await app.db('categories')
                 .where({ parentId: req.params.id })
-            notExistsOrError(subcategory, 'Categoria possui Subcategorias.')
+            notExistsOrError(subcategory, 'Categoria possui subcategorias.')
 
             const articles = await app.db('articles')
                 .where({ categoryId: req.params.id })
@@ -88,5 +93,23 @@ module.exports = app => {
             .catch(err => res.status(500).send(err))
     }
 
-    return { save, remove, get, getById }
+    const toTree = (categories, tree) => {
+        if (!tree) tree = categories.filter(c => !c.parentId)
+        tree = tree.map(parentNode => {
+            const isChild = node => node.parentId == parentNode.id
+            parentNode.children = toTree(categories, categories.filter(isChild))
+            return parentNode
+        })
+
+        return tree
+    }
+
+    const getTree = (req, res) => {
+        app.db('categories')
+            .then(categories => res.json(toTree(categories)))
+            .catch(err => res.status(500).send(err))
+            // .then(categories => res.json(toTree(withPath(categories))))
+    }
+
+    return { save, remove, get, getById, getTree }
 }
--- backend/config/routes.js
+++ backend/config/routes.js
@@ -16,6 +16,12 @@ module.exports = app => {
     app.route('/categories')
         .get(app.api.category.get)
         .post(app.api.category.save)
+    
+
+    // Cuidadocom a ordem! Tem que vir antes de /categories/:id
+
+    app.route('/categories/tree')
+        .get(app.api.category.getTree)
         
     app.route('/categories/:id')
         .get(app.api.category.getById)

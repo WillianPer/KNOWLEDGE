21cae55d6c39b2bcf4e4882e56b95f67ff5b5901
WilianPer <wsresilienttasteshopw@gmail.com>
2022-11-28 15:40:05 -0400

    Cadastro de Categoria #01!!

--- frontend/src/components/admin/CategoryAdmin.vue
+++ frontend/src/components/admin/CategoryAdmin.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="category-admin">
         <h1>Categoria Componente</h1>
-        <!-- <b-form>
+        <b-form>
             <input id="category-id" type="hidden" v-model="category.id" />
             <b-form-group label="Nome:" label-for="category-name">
                 <b-form-input id="category-name" type="text"
@@ -34,71 +34,71 @@
                     <i class="fa fa-trash"></i>
                 </b-button>
             </template>
-        </b-table> -->
+        </b-table>
     </div>
 </template>
 
 <script>
-// import { baseApiUrl, showError } from '@/global'
-// import axios from 'axios'
+import { baseApiUrl, showError } from '@/global'
+import axios from 'axios'
 
 export default {
     name: 'CategoryAdmin',
-    // data: function() {
-    //     return {
-    //         mode: 'save',
-    //         category: {},
-    //         categories: [],
-    //         fields: [
-    //             { key: 'id', label: 'Código', sortable: true },
-    //             { key: 'name', label: 'Nome', sortable: true },
-    //             { key: 'path', label: 'Caminho', sortable: true },
-    //             { key: 'actions', label: 'Ações' }
-    //         ]
-    //     }
-    // },
-    // methods: {
-    //     loadCategories() {
-    //         const url = `${baseApiUrl}/categories`
-    //         axios.get(url).then(res => {
-    //             // this.categories = res.data
-    //             this.categories = res.data.map(category => {
-    //                 return { ...category, value: category.id, text: category.path }
-    //             })
-    //         })
-    //     },
-    //     reset() {
-    //         this.mode = 'save'
-    //         this.category = {}
-    //         this.loadCategories()
-    //     },
-    //     save() {
-    //         const method = this.category.id ? 'put' : 'post'
-    //         const id = this.category.id ? `/${this.category.id}` : ''
-    //         axios[method](`${baseApiUrl}/categories${id}`, this.category)
-    //             .then(() => {
-    //                 this.$toasted.global.defaultSuccess()
-    //                 this.reset()
-    //             })
-    //             .catch(showError)
-    //     },
-    //     remove() {
-    //         const id = this.category.id
-    //         axios.delete(`${baseApiUrl}/categories/${id}`)
-    //             .then(() => {
-    //                 this.$toasted.global.defaultSuccess()
-    //                 this.reset()
-    //             })
-    //             .catch(showError)
-    //     },
-    //     loadCategory(category, mode = 'save') {
-    //         this.mode = mode
-    //         this.category = { ...category }
-    //     }
-    // },
-    // mounted() {
-    //     this.loadCategories()
-    // }
+    data: function() {
+        return {
+            mode: 'save',
+            category: {},
+            categories: [],
+            fields: [
+                { key: 'id', label: 'Código', sortable: true },
+                { key: 'name', label: 'Nome', sortable: true },
+                { key: 'path', label: 'Caminho', sortable: true },
+                { key: 'actions', label: 'Ações' }
+            ]
+        }
+    },
+    methods: {
+        loadCategories() {
+            const url = `${baseApiUrl}/categories`
+            axios.get(url).then(res => {
+                // this.categories = res.data
+                this.categories = res.data.map(category => {
+                    return { ...category, value: category.id, text: category.path }
+                })
+            })
+        },
+        reset() {
+            this.mode = 'save'
+            this.category = {}
+            this.loadCategories()
+        },
+        save() {
+            const method = this.category.id ? 'put' : 'post'
+            const id = this.category.id ? `/${this.category.id}` : ''
+            axios[method](`${baseApiUrl}/categories${id}`, this.category)
+                .then(() => {
+                    this.$toasted.global.defaultSuccess()
+                    this.reset()
+                })
+                .catch(showError)
+        },
+        remove() {
+            const id = this.category.id
+            axios.delete(`${baseApiUrl}/categories/${id}`)
+                .then(() => {
+                    this.$toasted.global.defaultSuccess()
+                    this.reset()
+                })
+                .catch(showError)
+        },
+        loadCategory(category, mode = 'save') {
+            this.mode = mode
+            this.category = { ...category }
+        }
+    },
+    mounted() {
+        this.loadCategories()
+    }
 }
 </script>
 

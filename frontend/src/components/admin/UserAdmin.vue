0d623bf8b9bca7de76129718e80c498f2078cf12
WilianPer <wsresilienttasteshopw@gmail.com>
2022-11-28 15:08:04 -0400

    Cadastro de Usuários #03!!

--- frontend/src/components/admin/UserAdmin.vue
+++ frontend/src/components/admin/UserAdmin.vue
@@ -5,34 +5,26 @@
             <b-row>
                 <b-col md="6" sm="12">
                     <b-form-group label="Nome:" label-for="user-name">
-                        <!-- <b-form-input id="user-name" type="text"
-                            v-model="user.name" required
-                            :readonly="mode === 'remove'"
-                            placeholder="Informe o Nome do Usuário..." /> -->
                         <b-form-input id="user-name" type="text"
                             v-model="user.name" required
+                            :readonly="mode === 'remove'"
                             placeholder="Informe o Nome do Usuário..." />
                     </b-form-group>
                 </b-col>
                 <b-col md="6" sm="12">
                     <b-form-group label="E-mail:" label-for="user-email">
-                        <!-- <b-form-input id="user-email" type="text"
-                            v-model="user.email" required
-                            :readonly="mode === 'remove'"
-                            placeholder="Informe o E-mail do Usuário..." /> -->
                         <b-form-input id="user-email" type="text"
                             v-model="user.email" required
+                            :readonly="mode === 'remove'"
                             placeholder="Informe o E-mail do Usuário..." />
                     </b-form-group>
                 </b-col>
             </b-row>
-            <!-- <b-form-checkbox id="user-admin" v-show="mode === 'save'"
-                v-model="user.admin" class="mt-3 mb-3"> -->
-            <b-form-checkbox id="user-admin" v-model="user.admin" class="mt-3 mb-3">
+            <b-form-checkbox id="user-admin" v-show="mode === 'save'"
+                v-model="user.admin" class="mt-3 mb-3">
                 Administrador?
             </b-form-checkbox>
-            <!-- <b-row v-show="mode === 'save'"> -->
-            <b-row>
+            <b-row v-show="mode === 'save'">
                 <b-col md="6" sm="12">
                     <b-form-group label="Senha:" label-for="user-password">
                         <b-form-input id="user-password" type="password"
@@ -61,14 +53,14 @@
         </b-form>
         <hr>
         <b-table hover striped :items="users" :fields="fields">
-            <!-- <template slot="actions" slot-scope="data">
+            <template slot="actions" slot-scope="data">
                 <b-button variant="warning" @click="loadUser(data.item)" class="mr-2">
                     <i class="fa fa-pencil"></i>
                 </b-button>
                 <b-button variant="danger" @click="loadUser(data.item, 'remove')">
                     <i class="fa fa-trash"></i>
                 </b-button>
-            </template> -->
+            </template>
         </b-table>
     </div>
 </template>
@@ -125,10 +117,10 @@ export default {
                 })
                 .catch(showError)
         },
-        // loadUser(user, mode = 'save') {
-        //     this.mode = mode
-        //     this.user = { ...user }
-        // }
+        loadUser(user, mode = 'save') {
+            this.mode = mode
+            this.user = { ...user }
+        }
     },
     mounted() {
         this.loadUsers()

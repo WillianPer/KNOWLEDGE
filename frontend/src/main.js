b4a2ea5c4af213cfd2bc4718ba0c10f495761aa6
WilianPer <wsresilienttasteshopw@gmail.com>
2022-11-27 23:34:05 -0400

    Comfigurando o VUE Toasted!!

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 
 import './config/bootstrap'
+import './config/msgs'
 import store from './config/store'
 
 
@@ -13,7 +14,7 @@ Vue.config.productionTip = false
 
 //  Temporário
 
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IldpbGxpYW4gUy4gUGVyZWlyYSIsImVtYWlsIjoid3NyZXNpbGllbnR0YXN0ZXNob3B3QGdtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2Njk1ODI1ODksImV4cCI6MTY2OTg0MTc4OX0.k7V46jfwtdJR3lVhvnAdI-So9cMaP6D3gZT63btFjew'
+// require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IldpbGxpYW4gUy4gUGVyZWlyYSIsImVtYWlsIjoid3NyZXNpbGllbnR0YXN0ZXNob3B3QGdtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2Njk1ODI1ODksImV4cCI6MTY2OTg0MTc4OX0.k7V46jfwtdJR3lVhvnAdI-So9cMaP6D3gZT63btFjew'
 
 new Vue({
   store,
